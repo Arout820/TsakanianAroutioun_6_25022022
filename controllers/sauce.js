e9b39396d9247092fd0335e9e746265a2e67def1
Aroutioun Tsakanian <aroutioun.tsakanian@gmail.com>
2022-03-08 04:31:36 +0100

    correction of a mistake on the controllers

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,57 +1,69 @@
-const Sauce = require('../models/Sauce');
+const Sauce = require("../models/Sauce");
 
-// ---------------- Ajoute une sauce dans un tableau de sauce ---------------- //
+// ---------------- Ajoute une sauce dans un tableau de sauces ---------------- //
 
 exports.createSauce = (req, res, next) => {
-    const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
-    console.log(req.body);
-    const sauce = new Sauce({
-        ...sauceObject,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
-    console.log(' ------SAUCE CREE-----');
-    console.log(sauce);
-    sauce.save()
-    .then(() => res.status(201).json({ message: 'Sauce enregistré' }))
-    .catch(error => res.status(400).json({ error }));
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  console.log(req.body);
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+  });
+  console.log(" ------SAUCE CREE-----");
+  console.log(sauce);
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Sauce enregistré" }))
+    .catch((error) => res.status(400).json({ error }));
 };
 
 // ----------------------- Renvoie tableau de sauces ----------------------- //
 
 exports.getAllSauces = (req, res, next) => {
-    Sauce.find()
-    .then(sauces => res.status(200).json({ sauces }))
-    .catch(error => res.status(400).json({ error: 'Aucune sauce disponible'}));
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) =>
+      res.status(400).json({ error: "Aucune sauce disponible" })
+    );
 };
 
 // --------------------------- Renvoie une sauce --------------------------- //
 
-// exports.getOneSauce = (req, res, next) => {
-//     Sauce.findOne({ _id: req.params.id })
-//     .then(sauce => res.status(200).json({ sauce }))
-//     .catch(error => res.status(400).json({ error: 'Cette sauce n\'existe pas'}));
-// };
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) =>
+      res.status(400).json({ error: "Cette sauce n'existe pas" })
+    );
+};
 
 // --------------------------- Supprime une sauce --------------------------- //
 
-// exports.deleteSauce = (req, res, next) => {
-//     Sauce.findOne({ _id: req.params.id })
-//     .then(sauce => {
-//         if (!sauce) {
-//             return res.status(404).json({ error: 'Sauce non trouvé' });
-//         }
-//         Sauce.deleteOne({ _id: req.params.id })
-//         .then(() => res.status(200).json({message: 'Sauce supprimé !'}))
-//         .catch(error => res.status(400).json({ error }));
-
-//     })
-//     .catch(error => res.status(400).json({ error: 'Cette sauce n\'existe pas'}));
-// };
-
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      if (!sauce) {
+        return res.status(404).json({ error: "Sauce non trouvé" });
+      }
+      Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Sauce supprimé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) =>
+      res.status(400).json({ error: "Cette sauce n'existe pas" })
+    );
+};
 
 // -------------------------- Modifie une sauce -------------------------- //
 
+exports.modifySauce = (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 // // ----------------------- LikeDislike d'une sauce ----------------------- //
 
@@ -60,7 +72,7 @@ exports.getAllSauces = (req, res, next) => {
 //     .then(objet => {
 //         const sauceId = {_id: req.params.id}
 //         console.log(sauceId);
-//         } 
+//         }
 //     })
 //     .catch(error => res.status(404).json({ error: 'holy shit' }));
 // };
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# Backend 
+# Backend
 
-Ici j'explique comment lancer le projet, les dépendances, et l'utilisation de l'api. 
+Ici j'explique comment lancer le projet, les dépendances, et l'utilisation de l'api.
 
 Pour relier le front et le back :
 Il faudra avoir la partie front clonable sur https://github.com/OpenClassrooms-Student-Center/Web-Developer-P6
@@ -21,15 +21,17 @@ Dépendances :
   npm i --save jsonwebtoken
   npm i --save multer
 ```
+
 Dépendances Dév:
 
 ```bash
   npm i --save-dev nodemon
 ```
-Résumé : 
+
+Résumé :
 
 - express
-- mongoose 
+- mongoose
 - mongoose-unique-validator
 - dotenv
 - nodemon
@@ -45,7 +47,7 @@ Il faudra créer un fichier .env et le mettre dans .gitignore
 
 Dans ce fichier crée les variables et on modifie les <> à modifier par les informations demandés
 
-``` 
+```
 PORT = <Le port utilisé 3000 la plupart du temps>
 DB_USERNAME = <Le nom d'utilisateur de la base de donnée>
 DB_PASSWORD = <Le mot de passe associé à l'utilisateur>
@@ -56,15 +58,14 @@ TOKEN = <Clé de chiffrement à créer permettant d'encrypter>
 
 Fonctionneront lorsque le serveur sera lancé avec node server ou nodemon server
 
-Pour s'inscrire : 
+Pour s'inscrire :
 http://localhost:3000/api/auth/signup
 
 Pour se connecter :
 http://localhost:3000/api/auth/login
 
-Pour retrouver les sauces : 
+Pour retrouver les sauces :
 http://localhost:3000/api/sauces
 
-Pour retrouver une sauce en particulier : 
+Pour retrouver une sauce en particulier :
 http://localhost:3000/api/sauces:id
-
